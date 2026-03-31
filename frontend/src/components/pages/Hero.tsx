897091834ed9a6dc94969738bd1a16313697413d
itsdevkinto <yo.kinto.x@gmail.com>
2026-03-31 21:46:32 +0800

    feat(frontend): improve homepage
    - Center hero section
    - Make the div of nav-links wider

--- frontend/src/components/pages/Hero.tsx
+++ frontend/src/components/pages/Hero.tsx
@@ -5,10 +5,20 @@ export default function Hero() {
         <h1 className="text-3xl font-bold mb-4">Welcome</h1>
         <p className="text-lg mb-4">Please login or register</p>
         <div className="flex flex-col gap-2 *:w-full w-full justify-center">
-          <a href="/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">Login</a>
-          <a href="/register" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Register</a>
+          <a
+            href="/login"
+            className="flex justify-center items-center bg-accent hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+          >
+            Login
+          </a>
+          <a
+            href="/register"
+            className="flex justify-center items-center bg-accent hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Register
+          </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -1,11 +1,11 @@
 
 export default function Header() {
   return (
-    <header className="sticky w-full top-0 bg-slate-700 text-white p-4 z-10">
-      <div className="container flex justify-between items-center mx-auto">
-        <div className="left font-bold">MERN Auth</div>
-        <div className=" flex items-center">
-          <a href="/login" className="mr-4">Login</a>
+    <header className="sticky w-full top-0 bg-slate-800 text-white py-4 z-10">
+    <div className="flex justify-between items-center px-6">
+        <div className="font-bold">MERN Auth</div>
+        <div className="flex items-center gap-4">
+          <a href="/login">Login</a>
           <a href="/register">Register</a>
         </div>
       </div>
